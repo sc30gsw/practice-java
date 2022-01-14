5844c8de60e9803df26d1a093f92c3e601cd4326
sc30gsw <haidoushi875@gmail.com>
2022-01-15 06:08:42 +0900

    catchブロックの検索

---
+++ src/com/example/demo/ExceptionExample.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+class SimpleClass {
+	void doSomething() {
+		int array[] = new int[3];
+		array[10] = 99;
+		System.out.println("doSometihngメソッドを終了します");
+	}
+}
+public class ExceptionExample {
+	public static void main(String args[]) {
+		SimpleClass obj = new SimpleClass();
+		try {
+			obj.doSomething();
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("例外をキャッチしました");
+			
+			//メソッドの呼び出し履歴を出力
+			e.printStackTrace();
+		}
+	}
+
+}
