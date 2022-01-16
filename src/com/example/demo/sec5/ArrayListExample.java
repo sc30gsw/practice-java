483a78578bd6fec145ca0d875bdb7b1c1987e064
sc30gsw <haidoushi875@gmail.com>
2022-01-17 08:51:30 +0900

    ArrayListのサンプル

---
+++ src/com/example/demo/sec5/ArrayListExample.java
@@ -0,0 +1,24 @@
+package com.example.demo.sec5;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+  public static void main(String[] args) {
+    ArrayList<String> months = new ArrayList<String>();
+
+    months.add("January");
+    months.add("Febrary");
+    months.add("March");
+
+    System.out.println("要素数" + months.size());
+    for(int i = 0; i < months.size(); i ++) {
+      System.out.println(months.get(i));
+    }
+
+    months.remove(1);
+    System.out.println("要素数" + months.size());
+    for(int i = 0; i < months.size(); i ++) {
+      System.out.println(months.get(i));
+    }
+  }
+}
