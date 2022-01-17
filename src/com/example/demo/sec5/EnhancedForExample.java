81be159f67a1de146fbf9820dc2a63f8ed8e282a
sc30gsw <haidoushi875@gmail.com>
2022-01-18 06:08:37 +0900

    拡張for文のサンプル作成

---
+++ src/com/example/demo/sec5/EnhancedForExample.java
@@ -0,0 +1,10 @@
+package com.example.demo.sec5;
+
+public class EnhancedForExample {
+  public static void main(String[] args) {
+    String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun"};
+    for(String str : months) {
+      System.out.println(str);
+    }
+  }
+}
