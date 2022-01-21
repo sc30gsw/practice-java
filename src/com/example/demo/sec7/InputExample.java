8ba456a64460bc6d12fcf1d3f79e08b7a1be7e08
sc30gsw <haidoushi875@gmail.com>
2022-01-22 07:54:51 +0900

    型変換を用いた入出力

--- src/com/example/demo/sec7/InputExample.java
+++ src/com/example/demo/sec7/InputExample.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class InputExample {
-  public static void main(String[] args) {
-    System.out.println("あなたのお名前は？");
+  public static void main(String[] args) {11
     InputStreamReader in = new InputStreamReader(System.in);
     BufferedReader reader = new BufferedReader(in);
     try {
-      String name = reader.readLine();
-      System.out.println("こんにちは。" + name + "さん");
+      String line = reader.readLine();
+      //文字列型をdouble型に変換
+      double val = Double.parseDouble(line);
+      System.out.println("入力された値の平方根は" + Math.sqrt(val));
       reader.close();
     } catch(IOException e) {
       System.out.println(e);
