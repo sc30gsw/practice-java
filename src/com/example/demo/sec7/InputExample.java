df50996a4343cb637ffcda93f23541b19f90c960
sc30gsw <haidoushi875@gmail.com>
2022-01-22 07:51:01 +0900

    標準入出力のサンプル作成

---
+++ src/com/example/demo/sec7/InputExample.java
@@ -0,0 +1,20 @@
+package com.example.demo.sec7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputExample {
+  public static void main(String[] args) {
+    System.out.println("あなたのお名前は？");
+    InputStreamReader in = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(in);
+    try {
+      String name = reader.readLine();
+      System.out.println("こんにちは。" + name + "さん");
+      reader.close();
+    } catch(IOException e) {
+      System.out.println(e);
+    }
+  }
+}
