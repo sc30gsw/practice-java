69266fe2279c425bf6d7bae7de1600ce763b206d
sc30gsw <haidoushi875@gmail.com>
2022-01-22 13:51:03 +0900

    ボタンの表示

--- src/com/example/demo/sec8/MyFrame.java
+++ src/com/example/demo/sec8/MyFrame.java
@@ -6,9 +6,10 @@ public class MyFrame extends JFrame {
   public static void main(String[] args) {
     new MyFrame();
   }
-  
+
   MyFrame() {
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    getContentPane().add(new JButton("ボタン"));
     setSize(300, 200);
     setVisible(true);
   }
