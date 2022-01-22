cc3aaf1447be2377d108be5a5fad1ed5ef056211
sc30gsw <haidoushi875@gmail.com>
2022-01-23 06:29:12 +0900

    グラデー村のある円形オブジェクトの作成サンプル

--- src/com/example/demo/sec9/MyPanel.java
+++ src/com/example/demo/sec9/MyPanel.java
@@ -1,26 +1,33 @@
 package com.example.demo.sec9;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import javax.swing.*;
 
 public class MyPanel extends JPanel {
   public void paintComponent(Graphics g) {
-    int N = 20; //円周上の点の数
-    int r = 200; //円の半径
-    int[] arrayX = new int[N];
-    int[] arrayY = new int[N];
+    Graphics2D g2 = (Graphics2D) g;
+    //アンチエイリアシングを有効にする
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    
+    int width = getWidth() - 20;
+    int height = getHeight() - 20;
 
-    //円周上のx,y座標値を計算して配列に格納
-    for (int i = 0; i < N; i++) {
-      arrayX[i] = (int) (250 + r * Math.cos(2 * Math.PI * i / N));
-      arrayY[i] = (int) (250 + r * Math.sin(2 * Math.PI * i / N));
-    }
+    //円形のオブジェクトを生成
+    //グラデーションの設定
+    GradientPaint paint = new GradientPaint(0f, 10f, Color.WHITE, 0f, (float)height, Color.BLACK);
+    g2.setPaint(paint);
 
-    //円周上の2点を結ぶ直線を描画
-    for (int i = 0; i < N; i++) {
-      for (int j = i + 1; j < N; j++) {
-        g.drawLine(arrayX[i], arrayY[i], arrayX[j], arrayY[j]);
-      }
-    }
+    //グラデーションの設定
+    Ellipse2D shape = new Ellipse2D.Double(10, 10, width, height);
+    //円形のオブジェクトを描画
+    g2.fill(shape);
+
+    //描画色を黒に設定
+    g2.setColor(Color.BLACK);
+    //線の太さを3にする
+    g2.setStroke(new BasicStroke(3));
+    //円形のオブジェクトの輪郭線を描画
+    g2.draw(shape);
   }
 }
