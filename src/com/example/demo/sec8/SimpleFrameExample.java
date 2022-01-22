3da3ae09a90495aa7a397356be644933300819b8
sc30gsw <haidoushi875@gmail.com>
2022-01-22 13:40:21 +0900

    フレームの表示サンプルの作成

---
+++ src/com/example/demo/sec8/SimpleFrameExample.java
@@ -0,0 +1,12 @@
+package com.example.demo.sec8;
+
+import javax.swing.*;
+
+public class SimpleFrameExample {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300, 200);
+    frame.setVisible(true);
+  }
+}
