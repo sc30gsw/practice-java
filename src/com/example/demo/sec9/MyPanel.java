86b3d1913c7b2787302d6602823d2a7cc6316d52
sc30gsw <haidoushi875@gmail.com>
2022-01-23 06:03:01 +0900

    色指定の描画サンプル作成

--- src/com/example/demo/sec9/MyPanel.java
+++ src/com/example/demo/sec9/MyPanel.java
@@ -5,8 +5,9 @@ import javax.swing.*;
 
 public class MyPanel extends JPanel {
   public void paintComponent(Graphics g) {
-    Dimension d = getSize();
-    g.drawLine(0, 0, d.width, d.height);
-    g.drawLine(0, d.height, d.width, 0);
+    g.setColor(Color.RED);
+    g.fillRect(10, 10, 100, 200);
+    g.setColor(new Color(128, 200, 255));
+    g.fillRect(120, 10, 100, 100);
   }
 }
