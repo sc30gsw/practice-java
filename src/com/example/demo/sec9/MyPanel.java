cd62bb8a9c47374e36f6a1f32698c9b9b9b09116
sc30gsw <haidoushi875@gmail.com>
2022-01-23 05:56:33 +0900

    長方形描画のサンプル作成

---
+++ src/com/example/demo/sec9/MyPanel.java
@@ -0,0 +1,10 @@
+package com.example.demo.sec9;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MyPanel extends JPanel {
+  public void paintComponent(Graphics g) {
+    g.fillRect(50, 30, 150, 100);
+  }
+}
