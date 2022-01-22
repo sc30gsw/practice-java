8ad7aa2dae525b4548606674bc38247506bb937c
sc30gsw <haidoushi875@gmail.com>
2022-01-23 06:12:41 +0900

    幾何学模様の描画サンプル作成

--- src/com/example/demo/sec9/MyPanel.java
+++ src/com/example/demo/sec9/MyPanel.java
@@ -5,9 +5,22 @@ import javax.swing.*;
 
 public class MyPanel extends JPanel {
   public void paintComponent(Graphics g) {
-    g.setColor(Color.RED);
-    g.fillRect(10, 10, 100, 200);
-    g.setColor(new Color(128, 200, 255));
-    g.fillRect(120, 10, 100, 100);
+    int N = 20; //円周上の点の数
+    int r = 200; //円の半径
+    int[] arrayX = new int[N];
+    int[] arrayY = new int[N];
+
+    //円周上のx,y座標値を計算して配列に格納
+    for (int i = 0; i < N; i++) {
+      arrayX[i] = (int) (250 + r * Math.cos(2 * Math.PI * i / N));
+      arrayY[i] = (int) (250 + r * Math.sin(2 * Math.PI * i / N));
+    }
+
+    //円周上の2点を結ぶ直線を描画
+    for (int i = 0; i < N; i++) {
+      for (int j = i + 1; j < N; j++) {
+        g.drawLine(arrayX[i], arrayY[i], arrayX[j], arrayY[j]);
+      }
+    }
   }
 }
